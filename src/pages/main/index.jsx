ae6935ed097c5ac576f8fcbf0c812b3b32264d36
Joshua <Jrusli@csu.fullerton.edu>
2023-10-07 18:18:07 -0700

    done

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,7 +1,22 @@
 export const Main = () => { 
     return ( 
     <div>
-       <h1>Hello world</h1>
+          <h1>Jokes!</h1>
+        <p>Welcome to my joke website</p>
+        <h2>Terms</h2>
+        <p>We require an age check to determin what types of jokes you will be able to view.
+            Thank you!</p>
+        <div class="form-group">
+            <label for="flavorShot">Age Requirement</label>
+                <select id="age-check" class="form-control" name="age">
+                <option value="">None</option>
+                <option value="yonger-18">Yonger then 18</option>
+                <option value="older-18">Older then 18</option>
+                </select>
+                <div>
+                    <button type="submit" class="btn btn-default">Submit</button>
+                </div>
+        </div>
     </div>
     )
 }
