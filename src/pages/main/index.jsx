3deea3b9bdb581cb94d42ec605f1928bcaa93b0e
hellokev <2kevinloi8@gmail.com>
2023-10-16 10:58:25 -0700

    Added jokes to Firebase database categorized by age group

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { getDatabase, set, get, update, remove, ref, child } from "firebase/database";
+
 
 export const Main = () => {
     const [age, setAge] = useState(""); 
@@ -6,6 +8,9 @@ export const Main = () => {
     const [showOlderJokes, setShowOlderJokes] = useState(false); 
     const [userJoke, setUserJoke] = useState(""); 
     const [userJokes, setUserJokes] = useState([]); 
+    const [jokeId, setJokeId] = useState(0);
+
+    const db = getDatabase();
 
     const handleAgeChange = (event) => {
         const selectedAge = event.target.value;
@@ -25,8 +30,45 @@ export const Main = () => {
     }
 
     // Function to handle form submission
+    // This code is connected to the Firebase Database and stores jokes in a form of JSON into jokes/older-18 or jokes/younger-18.
     const handleSubmit = () => {
-        // Age-related actions can be performed here if needed
+        if (userJoke.trim() !== "" && age === "older-18") {
+            const newJoke = {
+                id: jokeId,
+                joke: userJoke,
+                rating: 0,
+                age: "+18"
+            };
+            setUserJokes([...userJokes, newJoke]);
+            setUserJoke("");
+
+            set(ref(db, `Jokes/older-18/${newJoke.id}`), newJoke)
+            .then(() => {
+                alert("Joke has been submitted!");
+            })
+            .catch((error) => {
+                alert(error);
+            });
+
+            setJokeId(jokeId + 1);
+        } else if (userJoke.trim() !== "" && age === "younger-18"){
+            const newJoke = {
+                id: jokeId,
+                joke: userJoke,
+                rating: 0,
+                age: "<18"
+            };
+            setUserJokes([...userJokes, newJoke]);
+            setUserJoke("");
+
+            set(ref(db, `Jokes/younger-18/${newJoke.id}`), newJoke)
+            .then(() => {
+                alert("Joke has been submitted!");
+            })
+            .catch((error) => {
+                alert(error);
+            });
+        }
     }
 
     // Function to handle user joke input
@@ -69,26 +111,49 @@ export const Main = () => {
                 <div>
                     <h2>Jokes for Younger Than 18</h2>
                     <p>Rate what you think is your most favorite joke!!</p>
-                    {/* Display jokes suitable for younger audiences */}
+                    {userJokes.map((jokeObj, index) => (
+                        jokeObj.age === "<18" ? (
+                            <div key={index}>
+                                <p>{jokeObj.joke}</p>
+                                <div>
+                                    <button onClick={() => rateUserJoke(index, 1)}>Like</button>
+                                    <button onClick={() => rateUserJoke(index, -1)}>Dislike</button>
+                                </div>
+                                <p>Rating: {jokeObj.rating === null ? "Not Rated" : jokeObj.rating}</p>
+                            </div>
+                        ) : null
+                    ))}
                 </div>
             )}
             {showOlderJokes && (
                 <div>
                     <h2>Jokes for 18 and Older</h2>
                     <p>Rate what you think is your most favorite joke!!</p>
-                    {/* Display jokes suitable for adults */}
+                    {userJokes.map((jokeObj, index) => (
+                        jokeObj.age === "+18" ? (
+                            <div key={index}>
+                                <p>{jokeObj.joke}</p>
+                                <div>
+                                    <button onClick={() => rateUserJoke(index, 1)}>Like</button>
+                                    <button onClick={() => rateUserJoke(index, -1)}>Dislike</button>
+                                </div>
+                                <p>Rating: {jokeObj.rating === null ? "Not Rated" : jokeObj.rating}</p>
+                            </div>
+                        ) : null
+                    ))}
                 </div>
             )}
+
             {/* Allow users to input jokes */}
             {(showYoungerJokes || showOlderJokes) && (
                 <div>
                     <h2>Submit Your Jokes</h2>
                     <input type="text" value={userJoke} onChange={handleUserJokeChange} />
-                    <button type="button" onClick={addUserJoke}>Submit Joke</button>
+                    <button type="button" onClick={handleSubmit}>Submit Joke</button>
                 </div>
             )}
             {/* Display user-submitted jokes */}
-            {(showYoungerJokes || showOlderJokes) && (
+            {/* {(showYoungerJokes || showOlderJokes) && (
                 <div>
                     <h2>User-Submitted Jokes</h2>
                     {userJokes.map((jokeObj, index) => (
@@ -102,7 +167,23 @@ export const Main = () => {
                         </div>
                     ))}
                 </div>
-            )}
+            )} */}
         </div>
     );
 }
+
+/* BUGS:
+-Ratings not updating into Firebase
+-*/
+
+/* CHANGES:
+BACKEND:
+    -Added jokes to Firebase database categorized by age group.
+        -Created two categories, "older-18" and "younger-18," to segregate jokes based on the age group.
+        -Added jokes to the "older-18" category with age "+18" and corresponding IDs, jokes, and initial ratings.
+        -Added jokes to the "younger-18" category with age "<18" and corresponding IDs, jokes, and initial ratings.
+FRONTEND:
+    -The younger/older joke filter wasn't working for me probably cause I broke the code some how but I added some changes to it so that it actually displays in this current code.
+    -Commented out "Display User-Submitted Jokes" for the meantime because it the filters weren't working for it.
+*/
+
