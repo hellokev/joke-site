cbb83c2c1cae77ab2f88ecd0e584678e21981fde
Joshua R <111922365+Jrusli@users.noreply.github.com>
2023-10-23 23:57:40 -0700

    Update index.jsx

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -2,31 +2,13 @@ import React, { useState } from 'react';
 
 export const Main = () => {
     const [age, setAge] = useState(""); 
-    const [showYoungerJokes, setShowYoungerJokes] = useState(false); 
-    const [showOlderJokes, setShowOlderJokes] = useState(false); 
     const [userJoke, setUserJoke] = useState(""); 
     const [userJokes, setUserJokes] = useState([]); 
 
+    // Function to handle age change
     const handleAgeChange = (event) => {
         const selectedAge = event.target.value;
         setAge(selectedAge);
-
-        // Determine which joke sections to show based on the selected age
-        if (selectedAge === "younger-18") {
-            setShowYoungerJokes(true);
-            setShowOlderJokes(false);
-        } else if (selectedAge === "older-18") {
-            setShowYoungerJokes(false);
-            setShowOlderJokes(true);
-        } else {
-            setShowYoungerJokes(false);
-            setShowOlderJokes(false);
-        }
-    }
-
-    // Function to handle form submission
-    const handleSubmit = () => {
-        // Age-related actions can be performed here if needed
     }
 
     // Function to handle user joke input
@@ -37,7 +19,7 @@ export const Main = () => {
     // Function to add user joke to the list
     const addUserJoke = () => {
         if (userJoke.trim() !== "") {
-            setUserJokes([...userJokes, { joke: userJoke, rating: null }]);
+            setUserJokes([...userJokes, { joke: userJoke, rating: null, age: age }]);
             setUserJoke(""); // Clear the input field
         }
     }
@@ -65,44 +47,46 @@ export const Main = () => {
             </div>
 
             {/* Conditionally render jokes based on the age selection */}
-            {showYoungerJokes && (
-                <div>
-                    <h2>Jokes for Younger Than 18</h2>
-                    <p>Rate what you think is your most favorite joke!!</p>
-                    {/* Display jokes suitable for younger audiences */}
-                </div>
-            )}
-            {showOlderJokes && (
+            {age === "older-18" && (
                 <div>
                     <h2>Jokes for 18 and Older</h2>
                     <p>Rate what you think is your most favorite joke!!</p>
                     {/* Display jokes suitable for adults */}
+                    <div>
+                        <h2>Submit Your Jokes</h2>
+                        <input type="text" value={userJoke} onChange={handleUserJokeChange} />
+                        <button type="button" onClick={addUserJoke}>Submit Joke</button>
+                    </div>
                 </div>
             )}
-            {/* Allow users to input jokes */}
-            {(showYoungerJokes || showOlderJokes) && (
+
+            {(age === "younger-18" || age === "") && (
                 <div>
-                    <h2>Submit Your Jokes</h2>
-                    <input type="text" value={userJoke} onChange={handleUserJokeChange} />
-                    <button type="button" onClick={addUserJoke}>Submit Joke</button>
-                </div>
-            )}
-            {/* Display user-submitted jokes */}
-            {(showYoungerJokes || showOlderJokes) && (
-                <div>
-                    <h2>User-Submitted Jokes</h2>
-                    {userJokes.map((jokeObj, index) => (
-                        <div key={index}>
-                            <p>{jokeObj.joke}</p>
-                            <div>
-                                <button onClick={() => rateUserJoke(index, 1)}>Like</button>
-                                <button onClick={() => rateUserJoke(index, -1)}>Dislike</button>
-                            </div>
-                            <p>Rating: {jokeObj.rating === null ? "Not Rated" : jokeObj.rating}</p>
+                    <h2>Jokes for Younger Than 18</h2>
+                    <p>Rate what you think is your most favorite joke!!</p>
+                    {/* Display jokes suitable for younger audiences */}
+                    {age === "younger-18" && (
+                        <div>
+                            <h2>Submit Your Jokes</h2>
+                            <input type="text" value={userJoke} onChange={handleUserJokeChange} />
+                            <button type="button" onClick={addUserJoke}>Submit Joke</button>
                         </div>
-                    ))}
+                    )}
                 </div>
             )}
+            
+            {/* Display user-submitted jokes based on age */}
+            {userJokes.map((jokeObj, index) => (
+                (age === "older-18" || jokeObj.age === "younger-18" || age === "") &&
+                <div key={index}>
+                    <p>{jokeObj.joke}</p>
+                    <div>
+                        <button onClick={() => rateUserJoke(index, 1)}>Like</button>
+                        <button onClick={() => rateUserJoke(index, -1)}>Dislike</button>
+                    </div>
+                    <p>Rating: {jokeObj.rating === null ? "Not Rated" : jokeObj.rating}</p>
+                </div>
+            ))}
         </div>
     );
 }
